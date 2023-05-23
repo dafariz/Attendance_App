ec737bce0aa6ac5a5536be8c908eaaccfc766542
simplilearn github <dafariz040302@gmail.com>
2023-05-24 00:38:51 +0800

    delete some comments on onboardingscreen.dart

--- lib/onboardingscreen.dart
+++ lib/onboardingscreen.dart
@@ -37,12 +37,6 @@ class OnboardingScreen extends StatelessWidget {
     ),
   ];
 
-  //const OnboardingScreen({super.key});
-
-  //const OnboardingScreen({super.key});
-
-  //const OnboardingScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
